051bf7d2e8b58bb870b51f25dd94cc017d740c90
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-05-17 04:18:59 +0600

    added popular menu section design

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
-    return (
-        <div>
-            <div>
-                <SectionTitle
-                    heading='FROM OUR MENU'
-                    subHeading='---Check it out---'
-                ></SectionTitle>
-            </div>
-        </div>
-    );
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItem = data.filter((item) => item.category === "popular");
+        setMenu(popularItem);
+      });
+  });
+
+  return (
+    <div className="mb-14">
+      <div>
+        <SectionTitle
+          heading="FROM OUR MENU"
+          subHeading="---Check it out---"
+        ></SectionTitle>
+      </div>
+
+      <div className="grid md:grid-cols-2 gap-4">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularMenu;
