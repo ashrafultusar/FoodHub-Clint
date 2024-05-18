6fc357f6dd7cc60a2da6e19af6a45efca8b74778
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-05-18 20:07:54 +0600

    added firebase initaial setup

---
+++ src/AuthProviders/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext=createContext(null)
+const auth = getAuth(app);
+
+
+const AuthProvider = ({ children }) => {
+    
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+
+
+
+    const authValue = {
+    user,loading
+}
+    return (
+        <AuthContext.Provider value={authValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
